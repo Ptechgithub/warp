2eca2f4d94a4bdae90277a693f44d1025bf36301
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-03-24 17:50:05 +0330

    update install.sh

--- endip/install.sh
+++ endip/install.sh
@@ -250,9 +250,9 @@ show() {
     echo -e "${green}$(v2ray)${rest}"
     echo ""
     echo -e "${purple}************************************${rest}"
-    echo -e "${blue}1) if you couldn't paste it in ${yellow}V2rayNG${yellow} or ${yellow}Nekobox${blue}, copy it to a text editor and remove any extra spaces.${rest}"
+    echo -e "${yellow}1) ${blue}if you couldn't paste it in ${yellow}V2rayNG${yellow} or ${yellow}Nekobox${blue}, copy it to a text editor and remove any extra spaces.${rest}"
     echo ""
-    echo -e "${blue}2) If you're using ${yellow}IPv6 in ${yellow}V2rayNG, place each IP inside${yellow} [ ] ${blue}example: ${yellow}[2606:4700:d0::1836:b925:ebb2:5eb1] ${rest}"
+    echo -e "${yellow}2) ${blue}If you're using ${yellow}IPv6 in ${yellow}V2rayNG, ${blue}place each IP inside${yellow} [ ] ${blue}example: ${yellow}[2606:4700:d0::1836:b925:ebb2:5eb1] ${rest}"
     echo -e "${purple}************************************${rest}"
 }
 
